f433b0118844e77c7365997e9429b2865d53e42e
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-05-16 12:35:24 +0700

    promise multiple file done

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function readFilePromise() {
+function readFilePromise(file) {
   // psst, the promise should be around here...
+  return new Promise((resolve,reject)=>{
+    fs.readFile(file,'utf-8',(err,dataFound)=>{
+      if(err) return reject('data \"'+file+'\" gak Ketemu');
+      dataFound = JSON.parse(dataFound)
+      resolve(dataFound)
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName)
+  .then(function(parents){
+    sleep.sleep(5)
+    return readFilePromise(childrenFileName)
+    .then(function(children){
+      return {parents:parents, children:children}
+    })
+    .catch(function(err){
+      console.log(err)
+    })
+  })
+  .then(function(parents_n_children){
+    for(let i = 0; i < parents_n_children.parents.length; i++){
+      parents_n_children.parents[i]['children'] = []
+      for(let j = 0; j < parents_n_children.children.length; j++){
+        if(parents_n_children.parents[i]['last_name'] === parents_n_children.children[j]['family']){
+          parents_n_children.parents[i]['children'].push(parents_n_children.children[j]['full_name'])
+        }
+      }
+    }
+    console.log(parents_n_children.parents)
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
